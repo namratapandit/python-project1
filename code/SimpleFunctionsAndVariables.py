c444bd2dae56c3fb2027f909c64a9afce99e383a
unknown <moviesartandme@gmail.com>
2019-05-01 11:21:42 +0530

    exercise1

---
+++ code/SimpleFunctionsAndVariables.py
@@ -0,0 +1,29 @@
+# simple functions using variables in python
+
+# defining global varaible
+printVariable = "shashank is the sweetest"
+
+# defining main function
+def main():
+    myfirstvariable = "Hello world !!"
+    print(printVariable)
+
+# calling the main function
+main()
+
+print("\n") # new line
+
+# defining another function printFunction
+def printFunction():
+    print(printVariable)
+
+# calling print function
+printFunction()
+
+# defining function printWithArgumentFunction with argument printArgument
+# argument is printArgument
+def printWithArgumentFunction(printArgument):
+    print(printArgument)
+
+# calling printWithArgumentFunction function
+printWithArgumentFunction("I am Namrata Pandit")
--- code/main.py
+++ code/main.py
@@ -1,2 +1,6 @@
-print("hello Shashank")
-print("having a good learning session Shashank?")
+# simple main() function to create print statements
+def main(): # creating a new function called main()
+    print( "Shashnak : Are you have a good learning session with me? \n "  # \n is new line
+           "Namu: Yes, I am !! ")
+
+main()  #this is calling the main function
